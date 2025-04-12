6be29b8a128084ec7c9c456b43b6e7d556afb14f
Vaibhav <vbhchaurasia001@gmail.com>
2025-04-12 14:49:24 +0530

    updated vite config

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -13,20 +13,20 @@ export const Contact = () => {
             Get In Touch
           </h2>
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all grid grid-cols-2 gap-4 text-center">
-          <a href="mailto:vbhchaurasia001@gmail.com" className="text-xl font-bold mb-4 flex items-center justify-center">
-              <img src="/pedrotech-portfolio/assets/mailLogo.png" alt="Mail" className="mr-2 w-6 h-6" />
+            <a href="mailto:vbhchaurasia001@gmail.com" className="text-xl font-bold mb-4 flex items-center justify-center">
+              <img src="/assets/mailLogo.png" alt="Mail" className="mr-2 w-6 h-6" />
               Mail
             </a>
             <a href="https://github.com/BitVaibhav" target="_blank" rel="noopener noreferrer" className="text-xl font-bold mb-4 flex items-center justify-center">
-              <img src="/pedrotech-portfolio/assets/githubLogo.png" alt="Github Logo" className="mr-2 w-6 h-6" />
+              <img src="/assets/githubLogo.png" alt="Github Logo" className="mr-2 w-6 h-6" />
               Github
             </a>
             <a href="https://www.linkedin.com/in/vaibhav-chaurasia-48536b1a1/" target="_blank" rel="noopener noreferrer" className="text-xl font-bold mb-4 flex items-center justify-center">
-              <img src="/pedrotech-portfolio/assets/linkedinLogo.png" alt="Linkedin Logo" className="mr-2 w-6 h-6" />
+              <img src="/assets/linkedinLogo.png" alt="Linkedin Logo" className="mr-2 w-6 h-6" />
               Linkedin
             </a>
             <a href="tel:+91 9327020998" className="text-xl font-bold mb-4 flex items-center justify-center">
-              <img src="/pedrotech-portfolio/assets/phoneLogo.png" alt="Phone" className="mr-2 w-6 h-6" />
+              <img src="/assets/phoneLogo.png" alt="Phone" className="mr-2 w-6 h-6" />
               +91 9327020998
             </a>
           </div>
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,7 +14,7 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold mb-2">CryptoAsk - React / API / JS</h3>
               <p className="text-gray-400 mb-4">
                 Scalable cloud infrastructure management with real-time
                 monitoring and automated scaling.
@@ -49,7 +49,7 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <h3 className="text-xl font-bold mb-2">Portfolio - React</h3>
               <p className="text-gray-400 mb-4">
                 ML-powered data visualization platform with predictive analytics
                 and interactive reports.
@@ -88,7 +88,7 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+              <h3 className="text-xl font-bold mb-2">InstaNotes - Flutter / SQFlite DB </h3>
               <p className="text-gray-400 mb-4">
                 Full-stack e-commerce with modern UI, secure payment
                 integration, and customizable product inventory.
@@ -129,7 +129,7 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <h3 className="text-xl font-bold mb-2">TechFest Registration Portal - DBMS</h3>
               <p className="text-gray-400 mb-4">
                 Scalable chat platform supporting real-time messaging, presence,
                 and group chat features.
@@ -154,6 +154,84 @@ export const Projects = () => {
                 <a
                   href="#"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all"
+            >
+              <h3 className="text-xl font-bold mb-2">Scraping Naukri.com - Web Scraping</h3>
+              <p className="text-gray-400 mb-4">
+                Scalable chat platform supporting real-time messaging, presence,
+                and group chat features.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all"
+            >
+              <h3 className="text-xl font-bold mb-2">WallPy - Flutter / API</h3>
+              <p className="text-gray-400 mb-4">
+                Scalable chat platform supporting real-time messaging, presence,
+                and group chat features.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="https://github.com/BitVaibhav/WallPy"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   View Project →
                 </a>
